897fbf111daec36eaae24f1720a1ae0f6fcae90a
aimanshahid30 <162031661+aimanshahid30@users.noreply.github.com>
2024-03-28 08:33:27 +0500

    Created a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWellcome to aiman's - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+let answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Enter your guessed number from 1 to 6 :",
+    }
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guessed correct number.");
+}
+else {
+    console.log("Ops! Sorry you guessed a wrong number");
+}
